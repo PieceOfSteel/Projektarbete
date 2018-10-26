eb9f0149a1ebeffbb211e14e99c732648ca8aa80
PieceOfSteel <43567912+PieceOfSteel@users.noreply.github.com>
2018-10-26 10:33:38 +0200

    Add files via upload

---
+++ Projekt_C/Core/Domain/PodcastEpisode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_C.Core.Domain
+{
+    class PodcastEpisode
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Projekt_C/Core/Domain/PodcastFeed.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_C.Core.Domain
+{
+    class PodcastFeed
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public int UpdateInterval { get; set; }
+        public List<PodcastEpisode> PodcastEpisodes { get; set; }
+    }
+}
