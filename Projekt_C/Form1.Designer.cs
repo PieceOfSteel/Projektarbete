7c0291b0cbfda06a42766b253dec9d33070eb411
Silfverlin <43538895+Silfverlin@users.noreply.github.com>
2018-10-30 16:35:25 +0100

    Add files via upload

--- Projekt_C/Form1.Designer.cs
+++ Projekt_C/Form1.Designer.cs
@@ -28,13 +28,172 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.lblUppdatering = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtBoxUrl = new System.Windows.Forms.TextBox();
+            this.lblUrl = new System.Windows.Forms.Label();
+            this.lblNamn = new System.Windows.Forms.Label();
+            this.txtBoxNamn = new System.Windows.Forms.TextBox();
+            this.lblKategori = new System.Windows.Forms.Label();
+            this.txtBoxKategori = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(825, 447);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(92, 39);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add feed";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(298, 74);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(399, 424);
+            this.listBox1.TabIndex = 5;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(820, 372);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(195, 69);
+            this.trackBar1.TabIndex = 6;
+            // 
+            // lblUppdatering
+            // 
+            this.lblUppdatering.AutoSize = true;
+            this.lblUppdatering.Location = new System.Drawing.Point(816, 332);
+            this.lblUppdatering.Name = "lblUppdatering";
+            this.lblUppdatering.Size = new System.Drawing.Size(169, 20);
+            this.lblUppdatering.TabIndex = 7;
+            this.lblUppdatering.Text = "Uppdaterings frekvens";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(298, 518);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 33);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Delete feed";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // txtBoxUrl
+            // 
+            this.txtBoxUrl.Location = new System.Drawing.Point(903, 223);
+            this.txtBoxUrl.Name = "txtBoxUrl";
+            this.txtBoxUrl.Size = new System.Drawing.Size(173, 26);
+            this.txtBoxUrl.TabIndex = 0;
+            this.txtBoxUrl.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // lblUrl
+            // 
+            this.lblUrl.AutoSize = true;
+            this.lblUrl.Location = new System.Drawing.Point(821, 226);
+            this.lblUrl.Name = "lblUrl";
+            this.lblUrl.Size = new System.Drawing.Size(50, 20);
+            this.lblUrl.TabIndex = 1;
+            this.lblUrl.Text = "URL :";
+            this.lblUrl.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblNamn
+            // 
+            this.lblNamn.AutoSize = true;
+            this.lblNamn.Location = new System.Drawing.Point(821, 170);
+            this.lblNamn.Name = "lblNamn";
+            this.lblNamn.Size = new System.Drawing.Size(59, 20);
+            this.lblNamn.TabIndex = 2;
+            this.lblNamn.Text = "Namn :";
+            // 
+            // txtBoxNamn
+            // 
+            this.txtBoxNamn.Location = new System.Drawing.Point(903, 167);
+            this.txtBoxNamn.Name = "txtBoxNamn";
+            this.txtBoxNamn.Size = new System.Drawing.Size(173, 26);
+            this.txtBoxNamn.TabIndex = 3;
+            // 
+            // lblKategori
+            // 
+            this.lblKategori.AutoSize = true;
+            this.lblKategori.Location = new System.Drawing.Point(821, 279);
+            this.lblKategori.Name = "lblKategori";
+            this.lblKategori.Size = new System.Drawing.Size(76, 20);
+            this.lblKategori.TabIndex = 8;
+            this.lblKategori.Text = "Kategori :";
+            // 
+            // txtBoxKategori
+            // 
+            this.txtBoxKategori.Location = new System.Drawing.Point(903, 276);
+            this.txtBoxKategori.Name = "txtBoxKategori";
+            this.txtBoxKategori.Size = new System.Drawing.Size(173, 26);
+            this.txtBoxKategori.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 20);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Categories :";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 74);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1097, 627);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtBoxKategori);
+            this.Controls.Add(this.lblKategori);
+            this.Controls.Add(this.lblUppdatering);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtBoxNamn);
+            this.Controls.Add(this.lblNamn);
+            this.Controls.Add(this.lblUrl);
+            this.Controls.Add(this.txtBoxUrl);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label lblUppdatering;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtBoxUrl;
+        private System.Windows.Forms.Label lblUrl;
+        private System.Windows.Forms.Label lblNamn;
+        private System.Windows.Forms.TextBox txtBoxNamn;
+        private System.Windows.Forms.Label lblKategori;
+        private System.Windows.Forms.TextBox txtBoxKategori;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- Projekt_C/Form1.cs
+++ Projekt_C/Form1.cs
@@ -16,5 +16,15 @@ namespace Projekt_C
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
