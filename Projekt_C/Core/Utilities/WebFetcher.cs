e2d7403016b2bf6ae8090e31cc6fdff461d8d0b7
PieceOfSteel <43567912+PieceOfSteel@users.noreply.github.com>
2018-10-31 17:52:32 +0100

    Add files via upload

--- Projekt_C/Core/Utilities/WebFetcher.cs
+++ Projekt_C/Core/Utilities/WebFetcher.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Projekt_C.Core.Domain;
+using System.Net.Http;
 
 namespace Projekt_C.Core.Utilities
 {
-    class WebFetcher
+    static public class WebFetcher
     {
+        private static HttpClient client = new HttpClient();
+        
+
+        public static Rss FetchRss(string url)
+        {
+            string content = Convert.ToString(FetchRssContent(url));
+
+            var rss = new Rss
+            {
+                Url = url,
+                Content = content
+            };
+
+            return rss;
+        }
+
+        public static async Task<String> FetchRssContent(string url)
+        {
+            return await client.GetStringAsync(url);
+            
+        }
     }
 }
